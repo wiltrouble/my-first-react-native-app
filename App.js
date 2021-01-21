c3d02aff15bc77a554341fc4c395de41b32a3a9b
wiltrouble <wiltrouble@gmail.com>
2021-01-21 17:30:25 -0400

    pick an image app finished

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import { Text, View, StyleSheet, Image } from "react-native";
+import React, {useState}from 'react'
+import { Text, View, StyleSheet, Image, Button, Alert, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
-const App = () => {
+const App = () => {  
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (permissionResult.granted === false) {
+      alert('permission to access camera is required!')
+      return;
+    }
+
+    const imageResult = await ImagePicker.launchImageLibraryAsync()
+
+    if (imageResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({localUri: imageResult.uri})
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello world!</Text>
+      <Text style={styles.title}>Pick an Image!</Text>
       <Image
-        source={{uri: 'https://picsum.photos/200/200'}}
+        source={{uri: selectedImage !== null ? selectedImage.localUri :'https://picsum.photos/200/200'}}
         style={styles.image}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => openImagePickerAsync()}
       >
-
-      </Image>
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
     </View>   
   )
 }
@@ -27,7 +51,17 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
-    borderRadius: 100
+    borderRadius: 100,
+    resizeMode: 'contain'
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "blueviolet",
+    padding: 10,
+    marginTop: 10
+  },
+  buttonText: {
+    color: '#fff'
   }
 })
 
